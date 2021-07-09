6ba482a5be0d8f388c3faa955800004917863ca7
Kozova1 <mug66kk@gmail.com>
2021-07-09 14:36:00 +0300

    Better error handling for all HttpClient usese
    Signed-off-by: Kozova1 <mug66kk@gmail.com>

--- src/main/kotlin/net/vogman/mcdeploy/DownloadURL.kt
+++ src/main/kotlin/net/vogman/mcdeploy/DownloadURL.kt
@@ -2,14 +2,20 @@ package net.vogman.mcdeploy
 
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 suspend fun ServerJarFetcher.DownloadURL.fetchImpl(config: Config): Result<ByteArray, Error> =
-    HttpClient().use { client ->
-        assert(config.Server.JarSource is ServerJarFetcher.DownloadURL)
-        println("Downloading server.jar from $ServerJarURL")
-        val response: HttpResponse = client.get(ServerJarURL)
-        val serverJar: ByteArray = response.receive()
-        return Result.Ok(serverJar)
+    try {
+        HttpClient().use { client ->
+            assert(config.Server.JarSource is ServerJarFetcher.DownloadURL)
+            println("Downloading server.jar from $ServerJarURL")
+            val response: HttpResponse = client.get(ServerJarURL)
+            val serverJar: ByteArray = response.receive()
+            return Result.Ok(serverJar)
+        }
+    } catch (e: ClientRequestException) {
+        logErr("Download failed: ${e.response.status.value} (${e.response.status.description})")
+        Result.Err(Error.Server)
     }
--- src/main/kotlin/net/vogman/mcdeploy/LauncherManifest.kt
+++ src/main/kotlin/net/vogman/mcdeploy/LauncherManifest.kt
@@ -2,58 +2,64 @@ package net.vogman.mcdeploy
 
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 suspend fun ServerJarFetcher.LauncherManifest.fetchImpl(config: Config): Result<ByteArray, Error> =
-    HttpClient().use { client ->
-        assert(config.Server.JarSource is ServerJarFetcher.LauncherManifest)
-        println("Downloading launcher manifest...")
-        val versionResponse: HttpResponse = client.get(LauncherManifestURL)
-        val versions: Versions = Json { ignoreUnknownKeys = true }.decodeFromString(versionResponse.receive())
-        val versionUrl = versions.findURI(
-            when {
-                Version.equals("latest-release", ignoreCase = true) -> versions.latest.release
-                Version.equals("latest-snapshot", ignoreCase = true) -> versions.latest.snapshot
-                else -> Version
+    try {
+        HttpClient().use { client ->
+            assert(config.Server.JarSource is ServerJarFetcher.LauncherManifest)
+            println("Downloading launcher manifest...")
+            val versionResponse: HttpResponse = client.get(LauncherManifestURL)
+            val versions: Versions = Json { ignoreUnknownKeys = true }.decodeFromString(versionResponse.receive())
+            val versionUrl = versions.findURI(
+                when {
+                    Version.equals("latest-release", ignoreCase = true) -> versions.latest.release
+                    Version.equals("latest-snapshot", ignoreCase = true) -> versions.latest.snapshot
+                    else -> Version
+                }
+            )
+
+            if (versionUrl == null) {
+                logErr("No manifest URL found for version $Version (Are you sure this is the correct version?)")
+                return Result.Err(Error.Server)
             }
-        )
 
-        if (versionUrl == null) {
-            logErr("No manifest URL found for version $Version (Are you sure this is the correct version?)")
-            return Result.Err(Error.Server)
+            println()
+
+            println("Downloading manifest for selected version...")
+            val responseManifest: HttpResponse = client.get(versionUrl)
+            val manifest: VersionManifest =
+                Json { ignoreUnknownKeys = true }.decodeFromString(responseManifest.receive())
+            logOk("Received manifest for version $Version")
+
+            if (manifest.downloads.server == null) {
+                logErr("No server jar for version $Version")
+                return Result.Err(Error.Server)
+            }
+
+            // Download server.jar for the selected version
+            println("Downloading server.jar...")
+            val serverResponse: HttpResponse = client.get(manifest.downloads.server.url)
+            val serverJar: ByteArray = serverResponse.receive()
+            logOk("Downloaded server.jar")
+            println("Verifying server.jar...")
+
+            val serverJarHash = sha1sum(serverJar)
+            println("Downloaded: $serverJarHash")
+            println("Manifest:   ${manifest.downloads.server.sha1}")
+            return if (manifest.downloads.server.sha1 == serverJarHash) {
+                logOk("SHA-1 Match! Continuing")
+                Result.Ok(serverJar)
+            } else {
+                logErr("SHA-1 Mismatch! Exiting")
+                Result.Err(Error.Hash)
+            }
         }
-
-        println()
-
-        println("Downloading manifest for selected version...")
-        val responseManifest: HttpResponse = client.get(versionUrl)
-        val manifest: VersionManifest =
-            Json { ignoreUnknownKeys = true }.decodeFromString(responseManifest.receive())
-        logOk("Received manifest for version $Version")
-
-        if (manifest.downloads.server == null) {
-            logErr("No server jar for version $Version")
-            return Result.Err(Error.Server)
-        }
-
-        // Download server.jar for the selected version
-        println("Downloading server.jar...")
-        val serverResponse: HttpResponse = client.get(manifest.downloads.server.url)
-        val serverJar: ByteArray = serverResponse.receive()
-        logOk("Downloaded server.jar")
-        println("Verifying server.jar...")
-
-        val serverJarHash = sha1sum(serverJar)
-        println("Downloaded: $serverJarHash")
-        println("Manifest:   ${manifest.downloads.server.sha1}")
-        return if (manifest.downloads.server.sha1 == serverJarHash) {
-            logOk("SHA-1 Match! Continuing")
-            Result.Ok(serverJar)
-        } else {
-            logErr("SHA-1 Mismatch! Exiting")
-            Result.Err(Error.Hash)
-        }
+    } catch (e: ClientRequestException) {
+        logErr("Download failed: ${e.response.status.value} (${e.response.status.description})")
+        Result.Err(Error.Server)
     }
